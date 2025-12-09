a465c23372a4da2244d229463d601badeed7ed9e
YMiLLYY <camillypereira2008@gmail.com>
2025-12-09 18:54:21 -0300

    Fix Sequelize dialect options for IPv4
    Updated Sequelize configuration to correct IPv6 setting.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const { Sequelize } = require('sequelize'); // <--- ESSA LINHA É ESSENCIAL
+const AnimalModel = require('./model/animal');
+const AdotanteModel = require('./model/adotante');
+
+require('dotenv').config();
+
 const sequelize = new Sequelize(process.env.SUPABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
@@ -5,6 +11,15 @@ const sequelize = new Sequelize(process.env.SUPABASE_URL, {
       require: true,
       rejectUnauthorized: false
     },
-    family: 4 // <--- ESSA LINHA PRECISA ESTAR LÁ
+    family: 4 // Correção do IPv6 para o Render
   }
 });
+
+const Animal = AnimalModel(sequelize);
+const Adotante = AdotanteModel(sequelize);
+
+// Configuração das relações
+Adotante.hasMany(Animal, { foreignKey: 'adotanteId', as: 'animais' });
+Animal.belongsTo(Adotante, { foreignKey: 'adotanteId', as: 'adotante' });
+
+module.exports = { sequelize, Sequelize, Animal, Adotante };
